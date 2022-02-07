a186ca4255633fb79122509ca775ba1912b07783
William Nogueira <will.ribeiro@live.com>
2022-02-07 18:48:39 -0300

    utilizando proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,10 +1,30 @@
 import React from "react";
-import { Container, Title } from "./styles"
+import { 
+  Container, 
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName,
+  
+} from "./styles"
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo source={{uri: 'https://avatars.githubusercontent.com/u/69291914?v=4'}}/>
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Rodrigo</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   );
 }
