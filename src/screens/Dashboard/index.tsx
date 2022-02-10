d19d42c833490787c6e9c6158322e3f29dd71fa0
William Nogueira <will.ribeiro@live.com>
2022-02-10 10:24:51 -0300

    tipando e acessando propriedades no styled components

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -31,9 +31,24 @@ export function Dashboard() {
       </Header>
 
       <HighlightCards>
-        <HighlightCard />
-        <HighlightCard />
-        <HighlightCard />
+        <HighlightCard 
+          type="up"
+          title="Entradas" 
+          amount="R$ 17.000.00" 
+          lastTransaction="Última entrada dia 13 de Abril de 2020"
+        />
+        <HighlightCard 
+          type="down"
+          title="Saídas" 
+          amount="R$ 16.600.00" 
+          lastTransaction="Última entrada dia 03 de Abril de 2020"
+        />
+        <HighlightCard 
+          type="total"
+          title="Total" 
+          amount="R$ 13.200.00" 
+          lastTransaction="Última entrada dia 7 de Abril de 2020"
+        />
       </HighlightCards >
     </Container>
   );
