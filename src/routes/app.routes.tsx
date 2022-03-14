6b3539cdb5062039726834f79650f8f2fd221ca8
William Nogueira <will.ribeiro@live.com>
2022-03-14 17:46:48 -0300

    criando interface de gráfico

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -7,8 +7,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 const {Navigator, Screen} = createBottomTabNavigator()
 
 import {Dashboard} from "../screens/Dashboard"
-
 import {Register} from "../screens/Register"
+import {Resume} from "../screens/Resume"
 
 export function AppRoutes() {
   const theme = useTheme();
@@ -54,7 +54,7 @@ export function AppRoutes() {
       />
       <Screen 
         name="Resumo"
-        component={Register}
+        component={Resume}
         options={{
           tabBarIcon: (({size, color}) => 
           <MaterialIcons 
