a13d42de6081f1beee123c17f3de8c55523ee4fc
William Nogueira <will.ribeiro@live.com>
2022-04-13 16:32:46 -0300

    conectando com uma conta Apple

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,6 +1,8 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
 
 import * as AuthSession from 'expo-auth-session';
+import * as AppleAuthentication from 'expo-apple-authentication';
+import { AsyncStorage } from "react-native";
 
 const {CLIENT_ID} = process.env;
 const {REDIRECT_URI} = process.env;
@@ -26,6 +28,7 @@ interface AuthorizationResponse {
 interface IAuthContextData {
   user: User;
   signInWithGoogle(): Promise<void>;
+  signInWithApple(): Promise<void>;
 }
 
 const AuthContext = createContext({} as IAuthContextData);
@@ -60,10 +63,38 @@ function AuthProvider({children}: AuthProviderProps) {
     }
   }
 
+  async function signInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME,
+          AppleAuthentication.AppleAuthenticationScope.EMAIL
+        ]
+      });
+
+      if(credential) {
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+          photo: undefined
+        }
+
+        setUser(userLogged);
+
+        await AsyncStorage.setItem('@gofinances:user', JSON.stringify(userLogged));
+      }
+
+    } catch (error) {
+      throw new Error(error)
+    }
+  }
+
   return (
     <AuthContext.Provider value={{
       user,
-      signInWithGoogle
+      signInWithGoogle,
+      signInWithApple
     }}>
       {children}
     </AuthContext.Provider>
--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -18,7 +18,7 @@ import {
 } from "./styles"
 
 export function SignIn() {
-  const {signInWithGoogle} = useAuth();
+  const {signInWithGoogle, signInWithApple} = useAuth();
 
   async function handleSignInWithGoogle() {
     try {
@@ -27,6 +27,16 @@ export function SignIn() {
     } catch (error) {
       console.log(error)
       Alert.alert('Não foi possível conectar a conta Google')
+    }
+  }
+
+  async function handleSignInWithApple() {
+    try {
+      await signInWithApple();
+      
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Não foi possível conectar a conta Apple')
     }
   }
 
@@ -54,7 +64,7 @@ export function SignIn() {
       <Footer>
         <FooterWrapper>
           <SignInSocialButton title='Entrar com Google' svg={GoogleSvg} onPress={handleSignInWithGoogle}/>
-          <SignInSocialButton title='Entrar com Apple' svg={AppleSvg}/>
+          <SignInSocialButton title='Entrar com Apple' svg={AppleSvg} onPress={handleSignInWithApple}/>
         </FooterWrapper>
       </Footer>
     </Container>
